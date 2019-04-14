c6d7d0f299d7fbd54c64a82dcfcc942f82c73e5a
Pedro Ramos <pedrorms3225@gmail.com>
2019-04-13 23:59:56 -0300

    adding layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
-  numFilms: number;
+export class HomePage implements OnInit {
+  public population: string;
+  public climate: string;
+  public terrain: string;
+  public numFilms: number;
+  public auxNum: number;
+
+  ngOnInit() {
+    this.getPlanet();
+  }
+
+  getRandomNum() {
+    // generate random number between 0 and 61;
+    return Math.floor(Math.random() * 61);
+  }
+
+  getPlanet() {
+    const rnd = this.getRandomNum();
+
+    if (this.auxNum !== rnd) {
+      this.auxNum = rnd;
+      // do shit
+    } else {
+      // re-run getRandomNum, avoid to display the same planet multiple times in a row;
+      this.getPlanet();
+    }
+
+    console.log('getPlanet', rnd);
+  }
 }
